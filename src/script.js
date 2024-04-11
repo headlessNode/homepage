580d3e063bafdb5cf6500d423d33146e9a27b085
headlessNode <harriskhan047@outlook.com>
2024-04-12 00:40:05 +0500

    Experience section UI overhaul - remove repository and live demo links - add short one word description of the project - add font-awesome arrow icon - add preview section and project preview images - animate preview section on mouse enter and leave - add slide-in animation to the arrow on mouse enter and leave - add scroll animation to the heading txt of experience section - add scroll animation to the projects container - add the required changes to the layout and page style

--- src/script.js
+++ src/script.js
@@ -20,6 +20,21 @@ const picture = document.querySelector('.picture');
 const mainTextOne = document.querySelector('.txt-one');
 const mainTextTwo = document.querySelector('.txt-two');
 
+const projectsContainer = document.querySelector('.experience .projects');
+const projectHeading = document.querySelector('.experience .proj-heading');
+const projectHeadingOne = projectHeading.querySelector('.proj-heading-one');
+const projectHeadingTwo = projectHeading.querySelector('.proj-heading-two');
+const projectPreview = document.querySelector(
+  '.projects .proj-preview .preview-img',
+);
+const projectBattleship = document.querySelector(
+  '.projects .proj-deets .battleship',
+);
+const projects = document.querySelectorAll('.projects .proj-deets .project');
+const projectTodo = document.querySelector('.projects .proj-deets .todo');
+const projectWeather = document.querySelector('.projects .proj-deets .weather');
+const projectTicTac = document.querySelector('.projects .proj-deets .tictac');
+
 const initialTimeline = gsap.timeline();
 
 initialTimeline
@@ -50,9 +65,8 @@ initialTimeline
 
 const scrollTimeline = gsap.timeline({
   scrollTrigger: {
-    // make ScrollTrigger part of the timeline
     trigger: '.hero',
-    pin: '.hero', // Pin everthing
+    pin: '.hero',
     scrub: true, // Use true to remove the lag that makes things feel janky
     start: 'top',
     end: 'bottom 50%',
@@ -108,4 +122,131 @@ textTwoScrollTimeline.from(mainTextTwo, {
   opacity: 0,
   y: 20,
   ease: 'none',
+});
+
+const projectHeadingScroll = gsap.timeline({
+  scrollTrigger: {
+    trigger: projectHeading,
+    start: 'top 50%%',
+    end: 'bottom 50%',
+  },
+});
+
+projectHeadingScroll.from([projectHeadingOne, projectHeadingTwo], {
+  y: 200,
+  opacity: 0,
+  duration: 1,
+  ease: 'power.in',
+});
+
+const projectsScrollTimeline = gsap.timeline({
+  scrollTrigger: {
+    trigger: projects,
+    start: 'top 50%',
+    end: 'bottom 50%',
+    markers: true,
+  },
+});
+
+projectsScrollTimeline.from(projectsContainer, {
+  y: 50,
+  opacity: 0,
+  ease: 'none',
+});
+
+function onMouseEnter(event) {
+  let buttonArrow;
+  if (event.target.classList.contains('battleship')) {
+    // projectPreview.innerHTML =
+    //   "<img class='battleship-img' style='background-size: contain' src='../src/img/battleship.jpeg'>";
+    projectPreview.style.backgroundImage = "url('../src/img/battleship.jpeg')";
+    projectPreview.style.backgroundSize = 'contain';
+    projectPreview.style.backgroundRepeat = 'no-repeat';
+    projectPreview.style.backgroundPosition = 'center';
+    buttonArrow = '.battleship button .arrow';
+  } else if (event.target.classList.contains('todo')) {
+    // projectPreview.innerHTML =
+    //   "<img class='todo-img' style='background-size: contain' src='../src/img/todo.jpeg'>";
+    projectPreview.style.backgroundImage = "url('../src/img/todo.jpeg')";
+    projectPreview.style.backgroundSize = 'contain';
+    projectPreview.style.backgroundRepeat = 'no-repeat';
+    projectPreview.style.backgroundPosition = 'center';
+    buttonArrow = '.todo button .arrow';
+  } else if (event.target.classList.contains('tictac')) {
+    // projectPreview.innerHTML =
+    //   "<img class='tictac-img' style='background-size: contain' src='../src/img/tic-tac-toe.jpeg'>";
+    projectPreview.style.backgroundImage = "url('../src/img/tic-tac-toe.jpeg')";
+    projectPreview.style.backgroundSize = 'contain';
+    projectPreview.style.backgroundRepeat = 'no-repeat';
+    projectPreview.style.backgroundPosition = 'center';
+    buttonArrow = '.tictac button .arrow';
+  } else {
+    // projectPreview.innerHTML =
+    //   "<img class='weather-img' style='background-size: contain' src='../src/img/weather.jpeg'>";
+    projectPreview.style.backgroundImage = "url('../src/img/weather.jpeg')";
+    projectPreview.style.backgroundSize = 'contain';
+    projectPreview.style.backgroundRepeat = 'no-repeat';
+    projectPreview.style.backgroundPosition = 'center';
+    buttonArrow = '.weather button .arrow';
+  }
+  const animationTimeline = gsap.timeline();
+  animationTimeline
+    .fromTo(
+      projectPreview,
+      {
+        opacity: 0,
+        y: 20,
+      },
+      {
+        opacity: 1,
+        y: 0,
+      },
+    )
+    .to(
+      buttonArrow,
+      {
+        x: 1,
+        width: '10%',
+      },
+      '<',
+    );
+}
+
+function onMouseLeave(event) {
+  let buttonArrow;
+  if (event.target.classList.contains('battleship')) {
+    buttonArrow = '.battleship button .arrow';
+  } else if (event.target.classList.contains('todo')) {
+    buttonArrow = '.todo button .arrow';
+  } else if (event.target.classList.contains('tictac')) {
+    buttonArrow = '.tictac button .arrow';
+  } else {
+    buttonArrow = '.weather button .arrow';
+  }
+  const animationTimeline = gsap.timeline();
+  animationTimeline
+    .fromTo(
+      projectPreview,
+      {
+        opacity: 1,
+        y: 0,
+      },
+      {
+        opacity: 0,
+        y: 20,
+      },
+    )
+    .to(
+      buttonArrow,
+      {
+        x: -20,
+        width: '0%',
+      },
+      '<',
+    );
+}
+
+projects.forEach((project) => {
+  project.addEventListener('mouseenter', onMouseEnter);
+  project.addEventListener('mouseleave', onMouseLeave);
 });
